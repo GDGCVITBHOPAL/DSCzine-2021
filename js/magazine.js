d60a398b224abb5c2c480df82345bf6830cc8fbe
abhishek2x <abhisheksrivastavabbn@gmail.com>
2021-05-30 23:42:49 +0530

    minified images

--- js/magazine.js
+++ js/magazine.js
@@ -48,7 +48,7 @@ function loadPage(page, pageElement) {
 
 	// Load the page
 
-	img.attr('src', 'pages/' + page + '.png');
+	img.attr('src', 'pages/' + page + '.jpg');
 
 	loadRegions(page, pageElement);
 
@@ -179,7 +179,7 @@ function loadLargePage(page, pageElement) {
 
 	// Loadnew page
 
-	img.attr('src', 'pages/' + page + '.png');
+	img.attr('src', 'pages/' + page + '.jpg');
 }
 
 // Load small page
@@ -193,7 +193,7 @@ function loadSmallPage(page, pageElement) {
 	img.unbind('load');
 	// Loadnew page
 
-	img.attr('src', 'pages/' + page + '.png');
+	img.attr('src', 'pages/' + page + '.jpg');
 }
 
 // http://code.google.com/p/chromium/issues/detail?id=128488
